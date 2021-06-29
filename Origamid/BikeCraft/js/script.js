e9535fe9f5855685902b144cc9d15f323e8762df
Lucas Matta <67201210+Lucas-Matta@users.noreply.github.com>
2021-06-29 11:00:01 -0300

    update sliders

---
+++ Origamid/BikeCraft/js/script.js
@@ -0,0 +1,17 @@
+new SimpleSlide({
+    slide: 'quote',
+    nav: false,
+    auto: true,
+    time: 3000,
+    pauseOnHover: false, // pausa a transição automática
+});
+
+new SimpleSlide({
+    slide: 'portfolio',
+    nav: true,
+    auto: true,
+    time: 5000,
+    pauseOnHover: false, // pausa a transição automática
+});
+
+
