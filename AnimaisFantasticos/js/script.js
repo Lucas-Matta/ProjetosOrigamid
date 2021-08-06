2201c0a7f2b46c0a0f158468d3a7162b4fe9b58b
Lucas Matta <67201210+Lucas-Matta@users.noreply.github.com>
2021-08-06 10:44:06 -0300

    update

--- AnimaisFantasticos/js/script.js
+++ AnimaisFantasticos/js/script.js
@@ -139,26 +139,52 @@ copy.appendChild(cloneMenu);
 ////////////////////////////////////////////////////////////////////////////////////
 ///             Navegação por Tab              ///
 
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function initiTab(){
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-tabContent[0].classList.add('ativo');
+    tabContent[0].classList.add('ativo');
 
-if(tabMenu.length && tabContent.length ){
-    // Remove a classe ativo das section
-    function activeTab(index){
-        tabContent.forEach((section) => {
-            section.classList.remove('ativo');
+    if(tabMenu.length && tabContent.length ){
+        // Remove a classe ativo das section
+        function activeTab(index){
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            })
+        
+            tabContent[index].classList.add('ativo');
+        }
+        // Ativa a função na seciton que clicar
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            })
         })
-    
-        tabContent[index].classList.add('ativo');
     }
-    // Ativa a função na seciton que clicar
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            activeTab(index);
-        })
-    })
 }
 
+initiTab();
+
+////////////////////////////////////////////////////////////////////////////////////
+///             Accordion List                    ///
+
+function initiAccordion(){
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    
+    if(accordionList.length){
+        accordionList[0].classList.add('ativo');
+        accordionList[0].nextElementSibling.classList.add('ativo');
+
+        function activeAccordion(){
+            this.classList.toggle('ativo');
+            this.nextElementSibling.classList.toggle('ativo');
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        })
+    }
+};
+
+initiAccordion();
 
