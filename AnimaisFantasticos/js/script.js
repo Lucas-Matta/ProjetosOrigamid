3905bb2dcaa77bd797494ea6fd9eb04c9400dae4
Lucas Matta <67201210+Lucas-Matta@users.noreply.github.com>
2021-08-06 10:16:18 -0300

    update js

--- AnimaisFantasticos/js/script.js
+++ AnimaisFantasticos/js/script.js
@@ -135,3 +135,30 @@ const copy = document.querySelector('.copy');
 const cloneMenu = menuCopy.cloneNode(true);
 
 copy.appendChild(cloneMenu);
+
+////////////////////////////////////////////////////////////////////////////////////
+///             Navegação por Tab              ///
+
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+tabContent[0].classList.add('ativo');
+
+if(tabMenu.length && tabContent.length ){
+    // Remove a classe ativo das section
+    function activeTab(index){
+        tabContent.forEach((section) => {
+            section.classList.remove('ativo');
+        })
+    
+        tabContent[index].classList.add('ativo');
+    }
+    // Ativa a função na seciton que clicar
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+            activeTab(index);
+        })
+    })
+}
+
+
