c3609009f8db290dc11298fa70eb62afb047d5b4
Lucas Matta <67201210+Lucas-Matta@users.noreply.github.com>
2021-08-13 10:30:33 -0300

    update class

---
+++ AnimaisFantasticos/js/Aula/constructorFunction.js
@@ -0,0 +1,8 @@
+// Constructor Function
+function Carro(marca, ano, preco){
+    this.marca = marca,
+    this.ano = ano,
+    this.valor = preco
+};
+
+const honda = new Carro('Honda', 1990, 15000);
